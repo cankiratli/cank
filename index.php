9f7c4efe906e1c9b45e395ae9653fcfe28c4e4ce
Can <46088637+cankiratli@users.noreply.github.com>
2023-06-18 05:41:41 +0300

    Add files via upload
    İlk Php Html Css

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="UTF-8">
+        <title> Anasayfa | CanKıratlı </title>
+
+        <link rel="stylesheet" href="style.css">
+
+        <script src="https://kit.fontawesome.com/2f7bbc63a5.js"
+        crossorigin="anonymous"></script>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
+
+    </head>
+        <body>
+
+            <section id="menu">
+                <div id="logo">Can Kıratlı</div>
+                <nav>
+                    <a href""><i class="fa-solid fa-house ikon"></i>Anasayfa</a>
+                    <a href""><i class="fa-solid fa-circle-info ikon"></i>Özgeçmiş</a>
+                    <a href""><i class="fa-solid fa-graduation-cap ikon"></i>Eğitimler</a>
+                    <a href""><i class="fa-solid fa-envelopes-bulk ikon"></i>İletişim</a>
+                </nav>
+            </section>
+                <section id="anasayfa">
+                    <div id="black">
+
+                    </div>
+                    <div id="icerik">
+                        <h2>Can Kıratlı</h2>
+                        <hr width=300 .align=left>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                            Iusto impedit alias eaque officia dolor,
+                             aperiadsa
+                             dasdasd
+
+
+                             to impedit alias eaque officia dolor,
+                             aperiadsa
+                             dasdasd
+                             dsam rerum nudasmquam quis fugit vel harum eius quo ea
+                              at minus mollitia vero. Recusandae, nostrum
+                             dsam rerum nudasmquam quis fugit vel harum eius quo ea
+                              at minus mollitia vero. Recusandae, nostrum!</p>
+                    </div>
+                </section>
+        </body>
+        </html>
+
+
+<?php
+
+
+?>
